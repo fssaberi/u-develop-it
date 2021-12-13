d699a9229f3dccf9bdf422de413116df49735590
Fatemeh Brizendine <ssaberi324@yahoo.com>
2021-12-12 23:05:35 -0800

    added voter routes

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection'); 
 
 // Get all parties
-app.get('/api/parties', (req, res) => {
+router.get('/api/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
     db.query(sql, (err, rows) => {
         if (err) {
@@ -18,7 +18,7 @@ app.get('/api/parties', (req, res) => {
 });
 
 // Get a single party
-app.get('/api/party/:id', (req, res) => {
+router.get('/api/party/:id', (req, res) => {
     const sql = `SELECT * FROM parties WHERE id = ?`;
     const params = [req.params.id];
     db.query(sql, params, (err, row) => {
@@ -34,7 +34,7 @@ app.get('/api/party/:id', (req, res) => {
 });
 
 // Delete a party
-app.delete('/api/party/:id', (req, res) => {
+router.delete('/api/party/:id', (req, res) => {
     const sql = `DELETE FROM parties WHERE id = ?`;
     const params = [req.params.id];
     db.query(sql, params, (err, result) => {
